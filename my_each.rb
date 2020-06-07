969860c176afab3eb28b16c8bf1e4926df741696
Bruce Parker <detailsharks@gmail.com>
2020-06-07 20:24:11 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,12 @@
 collection = ("arel" "jon" "logan" "spencer") 
-def my_each(tas) do |ta|
+def my_each(tas) |ta|
   i = 0 
   while i < array.length 
   yield(array [i])
   i = i + 1 
 end end
 
-("arel" "jon" "logan" "spencer") do|
+("arel" "jon" "logan" "spencer") 
   
 
 
